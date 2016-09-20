414eecd2fc4b42d7d99ea4257ec1403916394443
zyguan <zhongyangguan@gmail.com>
2016-09-20 20:57:48 +0800

    rewrite logic

--- bot.js
+++ bot.js
@@ -12,23 +12,44 @@ function ocr(imgData) {
   valstr = valstr.replace(/[zZ]/g, '2');
   valstr = valstr.replace(/[sS]/g, '6');
   valstr = valstr.replace(/[oO]/g, '0');
+  valstr = valstr.replace(/[Il]/g, '1');
   valstr = valstr.replace(/B/g, '8');
   valstr = valstr.replace(/_/g, '4');
-  valstr = valstr.replace(/l/g, '1');
   valstr = valstr.replace(/g/g, '9');
   return valstr;
 }
 
+
+function tryGetAward(ctrl, timer, cnt) {
+  if (cnt > 0 && timer.text() == "00:00" &&
+      timer.parents('.treasure-box-ctnr').css('display') != "none") {
+    ctrl.captcha.refresh();
+    $('.acquiring-panel .captcha-img').one("load", function() {
+      try {
+        var val = eval(ocr(getImgData(this)));
+        console.log("tryGetAward("+cnt+"):", val, this);
+        ctrl.captcha.userInput = val;
+        ctrl.getAward();
+      } catch (err) {
+        console.error("GetAward:", err);
+      } finally {
+        setTimeout(function () {
+          tryGetAward(ctrl, timer, cnt-1);
+        }, 1500);
+      }
+    });
+  }
+}
+
 $('.box-doms').bind("DOMSubtreeModified", function() {
   var $boxDoms = $(this);
-  if ($boxDoms.find('.count-down').text() == "00:00") {
-    $boxDoms.find('.treasure-box').click();
-    setTimeout(function() {
-      var valstr = ocr(getImgData($('.captcha-img')[0]));
-      avalon.vmodels.treasureCtrl.captcha.userInput = eval(valstr);
-      console.log("submit:", eval(valstr), $('.captcha-img')[0]);
-      $('.get-award-btn').click();
-    }, 5000);
+  var $timer = $boxDoms.find('.count-down');
+  if ($timer.text() == "00:00") {
+    tryGetAward(avalon.vmodels.treasureCtrl, $timer, 5);
   }
 });
 
+// $('.acquiring-panel .captcha-img').on("load", function() {
+//   var val = eval(ocr(getImgData(this)));
+//   console.log("load captcha", val, this);
+// });
